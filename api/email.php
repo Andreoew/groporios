d9c09924c9fcddc630a1a7886e988e4f81d55d73
André S <andreoew2@gmail.com>
2022-05-21 23:43:43 -0300

    update

--- api/email.php
+++ api/email.php
@@ -7,20 +7,21 @@ $telefone = addslashes($_POST['telefone']);
 $email= addslashes($_POST['email']);
 $message = addslashes($_POST['message']);
 
-$to = "contato@gruporiosengenharia.com.br";
-$subject = "Contato - Grupo Rios Engenharia";
+$to = "contato@rosestrieder.mat.br";
+$subject = "Contato - Professora Rose";
 $body = "Nome: ".$name. "\r\n".
         "Telefone: ".$telefone. "\r\n".
         "Email: ".$email. "\r\n".
         "Mensagem: ".$message;
 
-$header = "From:gruporiosengenharia.com.br"."\r\n".
+$header = "From:rosestrieder.mat.br"."\r\n".
             "Reply-To:".$email."\e\n".
             "x=Mailer:PHP/".phpversion();
 
             
 if(mail($to,$subject,$body,$header)){
     echo("$name, O Email enviado com sucesso!");
+    header("Refresh: 5;url=https://gruporiosengenharia.com.br/");
     
 
 }else{
